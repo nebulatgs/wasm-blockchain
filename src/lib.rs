bec65111cd285c3bce4a00253994422b6419ee9a
Nebula <infinitewormhole@gmail.com>
2021-11-04 15:22:28 +0000

    Ability to recieve remote blockchain

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
 // sample code below was taken from https://github.com/rustwasm/wasm-bindgen
 
+extern crate blake3;
+extern crate serde;
+extern crate serde_json;
 extern crate wasm_bindgen;
+
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
 
 use std::{
     collections::hash_map::DefaultHasher,
     hash::{Hash, Hasher},
 };
+
 use wasm_bindgen::prelude::*;
 // JS Bindings
 #[wasm_bindgen]
@@ -18,9 +25,9 @@ extern "C" {
 }
 
 //Structs
-#[derive(Hash, Debug)]
+#[derive(Serialize, Deserialize, Hash, Debug, Clone)]
 pub struct Block {
-    pub hash: String,
+    // pub hash: String,
     pub previous_hash: String,
     pub timestamp: u32,
     pub data: String,
@@ -36,7 +43,7 @@ pub fn get_timestamp_in_millis() -> u128 {
 impl Block {
     pub fn new(data: String, previous_hash: String) -> Block {
         let mut block = Block {
-            hash: String::new(),
+            // hash: String::new(),
             previous_hash,
             timestamp: now(),
             data,
@@ -47,24 +54,24 @@ impl Block {
     }
 
     pub fn mine(&mut self) {
-        let mut nonce = 0;
         loop {
-            self.hash = self.calculate_hash();
-            if self.hash.ends_with("000000") {
+            let hash = self.calculate_hash();
+            // log(&hash);
+            if hash.starts_with("0000") {
                 break;
             }
-            nonce += 1;
+            self.nonce += 1;
         }
-        self.nonce = nonce;
     }
 
     pub fn calculate_hash(&self) -> String {
-        let mut s = DefaultHasher::new();
-        self.hash(&mut s);
-        s.finish().to_string()
+        let mut s = blake3::Hasher::new();
+        s.update_rayon(format!("{:?}", self).as_bytes());
+        s.finalize().to_hex().to_string()
     }
 }
 #[wasm_bindgen]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Blockchain {
     chain: Vec<Block>,
 }
@@ -83,7 +90,7 @@ impl Blockchain {
             self.chain.push(block);
             self.chain.last().unwrap()
         } else {
-            let previous_hash = last.unwrap().hash.clone();
+            let previous_hash = last.unwrap().calculate_hash();
             let block = Block::new(data, previous_hash);
             self.chain.push(block);
             self.chain.last().unwrap()
@@ -92,24 +99,142 @@ impl Blockchain {
     pub fn get_latest_block(&self) -> &Block {
         self.chain.last().unwrap()
     }
+    pub fn get_length(&self) -> usize {
+        self.chain.len()
+    }
+
+    pub fn verify(&self) -> bool {
+        let mut previous_hash = String::new();
+        for block in self.chain.iter() {
+            let hash = block.calculate_hash();
+            if !hash.starts_with("0000") {
+                return false;
+            }
+            if previous_hash != block.previous_hash {
+                return false;
+            }
+            previous_hash = hash;
+        }
+        true
+    }
 }
+
+#[wasm_bindgen]
+#[derive(Clone)]
+pub struct Chainholder {
+    chains: Vec<Blockchain>,
+    active_chain: usize,
+}
+
+impl Chainholder {
+    pub fn new(chains: Vec<Blockchain>) -> Chainholder {
+        let mut holder = Chainholder {
+            chains,
+            active_chain: 0,
+        };
+        holder.calculate_active_chain();
+        holder
+    }
+
+    pub fn get_active(&mut self) -> &mut Blockchain {
+        &mut self.chains[self.active_chain]
+    }
+
+    pub fn add_chain(&mut self, chain: Blockchain) -> &Blockchain {
+        self.chains.push(chain);
+        self.calculate_active_chain()
+    }
+
+    pub fn remove_chain(&mut self, i: usize) -> &Blockchain {
+        self.chains.remove(i);
+        self.calculate_active_chain()
+    }
+
+    pub fn calculate_active_chain(&mut self) -> &Blockchain {
+        let mut i = 0;
+        let mut max_length = 0;
+        for chain in self.chains.iter() {
+            if chain.get_length() > max_length {
+                max_length = chain.get_length();
+                i = self.chains.len() - 1;
+            }
+        }
+        self.active_chain = i;
+        self.get_active()
+    }
+}
+
 // Export a `greet` function from Rust to JavaScript, that alerts a
 // hello message.
 
 #[wasm_bindgen]
-pub fn setup() -> Blockchain {
-    Blockchain::new()
+pub fn setup() -> Chainholder {
+    Chainholder::new(vec![Blockchain::new()])
 }
 
 #[wasm_bindgen]
-pub fn add_block(chain: &mut Blockchain, data: &str) -> String {
+pub fn verify_chain_in_holder(holder: &mut Chainholder) -> bool {
+    let chain = holder.get_active();
+    chain.verify()
+}
+
+#[wasm_bindgen]
+pub fn get_chain_from_holder(holder: &mut Chainholder) -> String {
+    let chain = holder.get_active();
+    serde_json::to_string(chain).unwrap()
+}
+
+#[wasm_bindgen]
+pub fn add_block_to_holder(holder: &mut Chainholder, data: &str) {
+    let chain = holder.get_active();
+    log(data);
+    let block: Block = serde_json::from_str(data).unwrap();
+    let mut local_chain = chain.clone();
+    local_chain.add_block(block.clone());
+    if local_chain.verify() {
+        *chain = local_chain;
+        log(format!(
+            "Accepted remote block: {:?}\n New chain is {:?}",
+            block, chain
+        )
+        .as_str());
+    } else {
+        log(format!(
+            "Rejected remote block: {:?}\n Current chain is {:?}",
+            block, chain
+        )
+        .as_str());
+    }
+    // let then = now();
+    // let block = chain.create_block(data.to_string());
+    // log(&format!(
+    //     "Added block {:?}\n mined in {} ms",
+    //     block,
+    //     now() - then
+    // ));
+    // format!("{:?}", block)
+}
+
+#[wasm_bindgen]
+pub fn add_chain_to_holder(holder: &mut Chainholder, data: &str) {
+    let chain: Blockchain = serde_json::from_str(data).unwrap();
+    if !&chain.verify() {
+        log("Rejected remote chain");
+        return;
+    }
+    holder.add_chain(chain);
+    log("Accepted remote chain");
+}
+
+#[wasm_bindgen]
+pub fn submit_block_to_holder(holder: &mut Chainholder, data: &str) -> String {
+    let chain = holder.get_active();
     let then = now();
     let block = chain.create_block(data.to_string());
     log(&format!(
-        "Added block {:?}\n mined in {} ms",
-        block,
+        "Added block {}\n mined in {} ms",
+        serde_json::to_string(block).unwrap(),
         now() - then
     ));
-    format!("{:?}", block)
-    // alert(&format!("Added block {:?}", block));
+    serde_json::to_string(block).unwrap()
 }
